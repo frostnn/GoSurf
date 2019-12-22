daedd806b5eabf8a4b4f94577cbe5e5cb408a395
Artem <gradysnn92@yandex.ru>
2019-12-22 15:32:59 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
 
+
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrowL.svg" alt="" ></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrowR.svg" alt="" ></img>', 
+    asNavFor: '.slider-dots',
+  });
+
+
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+    
+  });
 });
