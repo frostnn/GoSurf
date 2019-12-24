8a70fc4ce979a86134cf82a0a6c56a7688bc3184
Artem <gradysnn92@yandex.ru>
2019-12-24 21:59:05 +0300

    Add new items

--- app/js/main.js
+++ app/js/main.js
@@ -6,14 +6,30 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrowL.svg" alt="" ></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrowR.svg" alt="" ></img>', 
-    asNavFor: '.slider-dots',
+    asNavFor: '.slider-dotshead',
   });
 
 
-  $('.slider-dots').slick({
+  $('.slider-dotshead ').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
     
+  });
+
+  $('.surf__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrowL.svg" alt="" ></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrowR.svg" alt="" ></img>',
+    asNavFor: '.slider-map',
+  });
+
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf__slider',
+    focusOnSelect: true,
   });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -49,6 +49,7 @@ gulp.task('css', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/animate.css/animate.css',
     /* 'node_modules/magnific-popup/dist/magnific-popup.css' */
   ])
   .pipe(concat('_libs.scss'))
